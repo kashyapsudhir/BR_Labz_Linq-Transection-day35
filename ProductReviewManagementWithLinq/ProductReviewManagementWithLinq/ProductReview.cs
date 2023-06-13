9b564d6dc210f783954b8412a5cfb19662c44dce
kashyapsudhir <kashyapsudhir1996@gmail.com>
2023-06-13 23:10:39 +0530

    [create product ]

---
+++ ProductReviewManagementWithLinq/ProductReviewManagementWithLinq/ProductReview.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagementWithLinq
+{
+    public class ProductReview
+    {
+        public int ProductID
+        {
+            get;
+            set;
+        }
+        public int UserID
+        {
+            get;
+            set;
+        }
+        public string Rating
+        {
+            get;
+            set;
+        }
+        public double Review
+        {
+            get;
+            set;
+        }
+        public bool IsLike
+        {
+            get;
+            set;
+        }
+    }
+}
--- ProductReviewManagementWithLinq/ProductReviewManagementWithLinq/Program.cs
+++ ProductReviewManagementWithLinq/ProductReviewManagementWithLinq/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
+using System.Linq;
+using System.Collections.Generic;
+using ProductReviewManagementWithLinq;
 
 namespace ProductReviewManagementWithlinq
 {
@@ -7,6 +11,90 @@ namespace ProductReviewManagementWithlinq
         static void Main(string[] args) 
         {
             Console.WriteLine("Hello Product review Management With Linq");
+            //UC-1
+            List<ProductReview> productReviews = new List<ProductReview>()
+            {
+                new ProductReview()
+                {
+                    ProductID=1,
+                    UserID=1,
+                    Rating="Good",
+                    Review= 5,
+                    IsLike=true
+                },
+                 new ProductReview()
+                {
+                    ProductID=2,
+                    UserID=3,
+                    Rating="Good",
+                    Review= 2,
+                    IsLike=true
+                },
+                  new ProductReview()
+                {
+                    ProductID=3,
+                    UserID=3,
+                    Rating="Nice",
+                    Review= 7,
+                    IsLike=false
+                },
+                   new ProductReview()
+                {
+                    ProductID=1,
+                    UserID=2,
+                    Rating="bad",
+                    Review= 4,
+                    IsLike=true
+                },
+                    new ProductReview()
+                {
+                    ProductID=4,
+                    UserID=3,
+                    Rating="nice",
+                    Review= 3,
+                    IsLike=false
+                },
+                     new ProductReview()
+                {
+                    ProductID=5,
+                    UserID=6,
+                    Rating="nice",
+                    Review= 5,
+                    IsLike=true
+                },
+                      new ProductReview()
+                {
+                    ProductID=5,
+                    UserID=6,
+                    Rating="Good",
+                    Review= 7,
+                    IsLike=true
+                },
+                       new ProductReview()
+                {
+                    ProductID=7,
+                    UserID=4,
+                    Rating="bad",
+                    Review= 2,
+                    IsLike=false
+                },
+                        new ProductReview()
+                {
+                    ProductID=8,
+                    UserID=7,
+                    Rating="Good",
+                    Review= 6,
+                    IsLike=false
+                },
+
+            }; foreach(var list in productReviews)
+            {
+                Console.WriteLine("ProductID:-" +list.ProductID + " " + "UserID:-" + list.UserID
+                    + " " + "Rating:-" + list.Rating + " " + "Review:-" + list.Review + " " + "IsLike:-" + list.IsLike);
+
+            }
         }
+
+
     }
 }
