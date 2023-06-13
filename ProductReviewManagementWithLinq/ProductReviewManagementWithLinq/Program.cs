48663ca7e937a6543fc37b2b6ca5a20d4989067e
kashyapsudhir <kashyapsudhir1996@gmail.com>
2023-06-13 22:16:08 +0530

    [Start Assignement day35]

---
+++ ProductReviewManagementWithLinq/ProductReviewManagementWithLinq/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProductReviewManagementWithlinq
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Hello Product review Management With Linq");
+        }
+    }
+}
